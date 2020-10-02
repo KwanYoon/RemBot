3d63992676c21418e15906349436f5b8bf0656d6
KwanYoon <47330978+KwanYoon@users.noreply.github.com>
2020-10-02 00:07:38 -0400

    Update rembot.py

--- rembot.py
+++ rembot.py
@@ -11,4 +11,10 @@ async def on_ready():
 async def ping(ctx):
     await ctx.send('Pong!')
 
+@client.event
+async def on_message(message):
+    channel = message.channel
+    if message.content.startswith('@405892011162337283'):
+        await client.send.send_message(channel, 'hehe')
+
 client.run('NDA1ODkyMDExMTYyMzM3Mjgz.WmkuZA.0dkWXVxX6wISApWCLYd3-4EVZEM')
