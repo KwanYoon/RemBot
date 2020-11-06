8db7dacb429694b7bdf7dc4eda7d59e9f25a462d
KwanYoon <47330978+KwanYoon@users.noreply.github.com>
2020-11-06 01:46:27 -0500

    Update rembot.py

--- rembot.py
+++ rembot.py
@@ -1,5 +1,14 @@
 import discord
 from discord.ext import commands
+import time
+import praw
+import urllib3
+
+reddit = praw.Reddit(
+     client_id="my client id",
+     client_secret="my client secret",
+     user_agent="my user agent"
+ )
 
 bot = commands.Bot(command_prefix = '.')
 
@@ -14,5 +23,9 @@ async def add(ctx, num1: float, num2: float):
 @bot.command()
 async def sub(ctx, num1: float, num2: float):
     await ctx.send(num1 - num2)
+
+@bot.command()
+async def reddit(ctx, subreddit: str):
+    await ctx.send(reddit.subreddit(subreddit))
 
 bot.run('NDA1ODkyMDExMTYyMzM3Mjgz.WmkuZA.0dkWXVxX6wISApWCLYd3-4EVZEM')
