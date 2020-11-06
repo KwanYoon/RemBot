18b04f430433f73b1c9c543dd15c9d8d9f166148
KwanYoon <47330978+KwanYoon@users.noreply.github.com>
2020-11-06 16:57:37 -0500

    Update rembot.py

--- rembot.py
+++ rembot.py
@@ -4,17 +4,17 @@ import time
 import praw
 import urllib3
 
-reddit = praw.Reddit(
-     client_id="my client id",
-     client_secret="my client secret",
-     user_agent="my user agent"
+red = praw.Reddit(
+     client_id="RlzWM0NK17wdOQ",
+     client_secret="mBs-_oP_lK7IxTaGJlgtiWlkGStILw",
+     user_agent="discord:RemBotv1.0:(By /u/rKeWdAiNt)"
  )
 
 bot = commands.Bot(command_prefix = '.')
 
 @bot.event
 async def on_ready():
-    print('Bot is ready.')
+    print(red.read_only)
 
 @bot.command()
 async def add(ctx, num1: float, num2: float):
@@ -25,7 +25,8 @@ async def sub(ctx, num1: float, num2: float):
     await ctx.send(num1 - num2)
 
 @bot.command()
-async def reddit(ctx, subreddit: str):
-    await ctx.send(reddit.subreddit(subreddit))
+async def reddit(ctx, subr: str):
+    for submission in red.subreddit(subr).top(limit=5):
+        await ctx.send(submission.url)
 
 bot.run('NDA1ODkyMDExMTYyMzM3Mjgz.WmkuZA.0dkWXVxX6wISApWCLYd3-4EVZEM')
