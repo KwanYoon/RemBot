22d2680357e0957231e1425a44a5105360cd2dbe
KwanYoon <47330978+KwanYoon@users.noreply.github.com>
2020-11-13 02:42:19 -0500

    Update rembot.py

--- rembot.py
+++ rembot.py
@@ -25,8 +25,30 @@ async def sub(ctx, num1: float, num2: float):
     await ctx.send(num1 - num2)
 
 @bot.command()
-async def reddit(ctx, subr: str):
-    for submission in red.subreddit(subr).top(limit=5):
-        await ctx.send(submission.url)
+async def reddit(ctx, subr: str, toptype: str, num: int):
+    if num > 10 or num < 1:
+        await ctx.send("Number has to be 1 ~ 10!")
+    elif (toptype != "hot" and toptype != "new" and toptype != "top"):
+        await ctx.send("The input has to be \"top\", \"new\", or \"hot\"!")
+    elif (toptype == "top"):
+        for submission in red.subreddit(subr).top(limit=num):
+            embedVar = discord.Embed(title="Title", description=submission.title, color=0xadd8e6)
+            embedVar.add_field(name="URL", value=submission.url, inline=False)
+            embedVar.set_image(url=submission.url)
+            await ctx.send(embed=embedVar)
+    elif (toptype == "new"):
+        for submission in red.subreddit(subr).new(limit=num):
+            embedVar = discord.Embed(title="Title", description=submission.title, color=0xadd8e6)
+            embedVar.add_field(name="URL", value=submission.url, inline=False)
+            embedVar.set_image(url=submission.url)
+            await ctx.send(embed=embedVar)
+    elif (toptype == "hot"):
+        for submission in red.subreddit(subr).hot(limit=num):
+            embedVar = discord.Embed(title="Title", description=submission.title, color=0xadd8e6)
+            embedVar.add_field(name="URL", value=submission.url, inline=False)
+            embedVar.set_image(url=submission.url)
+            await ctx.send(embed=embedVar)
+    else:
+        await ctx.send("Input is \".reddit subreddit type(top, new, hot) number\"")
 
 bot.run('NDA1ODkyMDExMTYyMzM3Mjgz.WmkuZA.0dkWXVxX6wISApWCLYd3-4EVZEM')
